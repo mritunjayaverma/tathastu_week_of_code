7a911d354eee4cc92502edd085ce0f1c282caa8e
mritunjayaverma <65852039+mritunjayaverma@users.noreply.github.com>
2020-05-27 15:28:48 +0530

    Create program5.py

---
+++ day1/program5.py
@@ -0,0 +1,12 @@
+rp1=int(input("enter run score by player 1"))
+rp2=int(input("enter run score by player 2"))
+rp3=int(input("enter run score by player 3"))
+str1=rp1*100/60
+str2=rp2*100/60
+str3=rp3*100/60
+print("number of maximum sixes by player 1:",str1)
+print("number of maximum sixes by player 2:",str2)
+print("number of maximum sixes by player 3:",str3)
+print ("if balls were 60 more then:",rp1*2)
+print ("if balls were 60 more then:",rp2*2)
+print ("if balls were 60 more then:",rp3*2)
